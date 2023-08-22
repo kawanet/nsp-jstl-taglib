a780a5c7fae4363e91c00904606a0f5f4ef00b92
Yusuke Kawasaki <u-suke@kawa.net>
2023-08-22 09:52:14 +0900

    TestSetTag

--- test/c-set.test.ts
+++ test/c-set.test.ts
@@ -21,4 +21,17 @@ describe(TITLE, () => {
 
         assert.equal(fn({}), "[FOO]");
     });
+
+    /**
+     * @see https://github.com/apache/tomcat-taglibs-standard/blob/main/standard-test/src/test/java/org/apache/taglibs/standard/tag/el/core/TestSetTag.java
+     */
+    it("TestSetTag", async () => {
+        const context = {var1: null as string};
+
+        const render = await nsp.loadJSP("test/resources/TestSetTag.jsp");
+        const result = await render(context);
+
+        assert.equal(result, "value1", "#1");
+        assert.equal(context.var1, "value1", "#2");
+    });
 });
