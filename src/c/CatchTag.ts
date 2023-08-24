df9d4e924bd188caba3da5318095d6f328de6871
Yusuke Kawasaki <u-suke@kawa.net>
2023-08-24 13:05:45 +0900

    <c:catch>

--- src/c/CatchTag.ts
+++ src/c/CatchTag.ts
@@ -1,6 +1,8 @@
 import type {NSP} from "nsp-server-pages";
 import type {JstlC} from "../index.js";
 
+const isPromise = <T>(v: any): v is Promise<T> => v && (typeof v.catch === "function");
+
 /**
  * <c:catch>
  * org.apache.taglibs.standard.tag.common.core.CatchTag
@@ -10,8 +12,23 @@ import type {JstlC} from "../index.js";
  * exposes it.
  */
 
-export const catchTag: NSP.TagFn<JstlC.CatchTagAttr> = _ => {
-    return _ => {
-        throw new Error("Not implemented: <c:catch>");
+export const catchTag: NSP.TagFn<JstlC.CatchTagAttr> = (tag) => {
+    return (context) => {
+        const {var: varName} = tag.attr(context);
+
+        const onCatch = (e: Error): undefined => {
+            context[varName] = e;
+        };
+
+        try {
+            const result = tag.body(context);
+            if (isPromise(result)) {
+                return result.catch(onCatch);
+            } else {
+                return result;
+            }
+        } catch (e) {
+            return onCatch(e);
+        }
     };
 };
---
+++ test/c-catch.test.ts
@@ -0,0 +1,47 @@
+import {strict as assert} from "assert";
+import {createNSP} from "nsp-server-pages";
+import {cTags} from "../src/index.js";
+
+const TITLE = "c-catch.test.ts";
+
+/**
+ * <c:catch>
+ */
+describe(TITLE, () => {
+    const nsp = createNSP({
+        indent: 4,
+    });
+
+    nsp.hook("error", (e) => {
+        throw e;
+    });
+
+    nsp.addTagLib({ns: "c", tag: cTags});
+
+    nsp.addTagLib({
+        ns: "test",
+        tag: {
+            throws: () => {
+                return () => {
+                    throw new Error("test error");
+                }
+            }
+        }
+    });
+
+    type Context = { err?: Error };
+
+    it("<c:catch>", async () => {
+        const src = '[<c:catch var="err"><test:throws/></c:catch>]';
+
+        const render = nsp.parse(src).toFn<Context>();
+
+        const ctx: Context = {};
+
+        const result = await render(ctx);
+
+        assert.equal(result, "[]");
+
+        assert.equal(ctx.err?.message, "test error");
+    });
+});
