97589e76001ab7b0c0c21a4d0944644a79c3cf96
Yusuke Kawasaki <u-suke@kawa.net>
2023-08-24 22:54:01 +0900

    <fmt:setTimeZone>

--- src/fmt/SetTimeZoneTag.ts
+++ src/fmt/SetTimeZoneTag.ts
@@ -1,5 +1,6 @@
 import type {NSP} from "nsp-server-pages";
 import type {JstlFmt} from "../index.js";
+import {TimeZone} from "../lib/TimeZone.js";
 
 /**
  * <fmt:setTimeZone>
@@ -8,8 +9,11 @@ import type {JstlFmt} from "../index.js";
  * @description
  * Stores the given time zone in the time zone configuration variable
  */
-export const setTimeZoneTag: NSP.TagFn<JstlFmt.SetTimeZoneTagAttr> = (_) => {
-    return (_) => {
-        throw new Error("Not implemented: <fmt:setTimeZone>");
+export const setTimeZoneTag: NSP.TagFn<JstlFmt.SetTimeZoneTagAttr> = (tag) => {
+    return (context) => {
+        const {value, var: varName} = tag.attr(context);
+        if (!value) throw new Error(`<fmt:setTimeZone> requires a "value" attribute`);
+
+        context[varName] = TimeZone.getTimeZone(value);
     };
 };
---
+++ test/fmt-setTimeZone.test.ts
@@ -0,0 +1,49 @@
+import {strict as assert} from "assert";
+import {createNSP} from "nsp-server-pages";
+import {fmtTags} from "../src/index.js";
+import type {TimeZone} from "../src/lib/TimeZone.js";
+
+const TITLE = "fmt-setTimeZone.test.ts";
+
+interface Context {
+    utc: Date;
+    tz?: string;
+    tzObj?: TimeZone;
+}
+
+/**
+ * <fmt:setTimeZone>
+ */
+describe(TITLE, () => {
+    const nsp = createNSP();
+
+    nsp.addTagLib({ns: "fmt", tag: fmtTags});
+
+    const ctx: Context = {
+        utc: new Date("2023-04-05T06:07:08Z"),
+    };
+
+    it('<fmt:setTimeZone>', async () => {
+        const src: string = '<fmt:setTimeZone value="${tz}" var="tzObj"/>[<fmt:formatDate value="${utc}" pattern="yyyy-MM-dd HH:mm:ss" timeZone="${tzObj}"/>]';
+        const render = nsp.parse(src).toFn<Context>();
+
+        let tz: string;
+        let expected: string;
+
+        ctx.tz = tz = "GMT";
+        expected = "[2023-04-05 06:07:08]";
+        assert.equal(await render(ctx), expected, tz);
+
+        ctx.tz = tz = "Asia/Tokyo";
+        expected = "[2023-04-05 15:07:08]";
+        assert.equal(await render(ctx), expected, tz);
+
+        ctx.tz = tz = "GMT+12:00";
+        expected = "[2023-04-05 18:07:08]";
+        assert.equal(await render(ctx), expected, tz);
+
+        ctx.tz = tz = "GMT-12:00";
+        expected = "[2023-04-04 18:07:08]";
+        assert.equal(await render(ctx), expected, tz);
+    });
+});
