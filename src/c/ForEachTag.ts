1d259ed902de946cad645ab59c59fa2b1abb4458
Yusuke Kawasaki <u-suke@kawa.net>
2023-08-24 13:22:09 +0900

    <c:forEach>

--- src/c/ForEachTag.ts
+++ src/c/ForEachTag.ts
@@ -11,8 +11,26 @@ import type {JstlC} from "../index.js";
  * functionality
  */
 
-export const forEachTag: NSP.TagFn<JstlC.ForEachTagAttr> = _ => {
-    return _ => {
-        throw new Error("Not implemented: <c:forEach>");
+export const forEachTag: NSP.TagFn<JstlC.ForEachTagAttr> = (tag) => {
+    return (context) => {
+        const attr = tag.attr(context);
+
+        const items = attr.items;
+        if (!items?.length) return;
+
+        const begin = +attr.begin || 0;
+        const end = +attr.end || items.length - 1;
+        const step = +attr.step || 1;
+        const varName = attr.var;
+
+        const results: (string | Promise<string>)[] = [];
+
+        for (let i = begin; i <= end; i += step) {
+            context[varName] = items[i];
+            const result = tag.body(context);
+            results.push(result);
+        }
+
+        return tag.app.concat(results);
     };
 };
---
+++ test/c-forEach.test.ts
@@ -0,0 +1,45 @@
+import {strict as assert} from "assert";
+import {createNSP} from "nsp-server-pages";
+import {cTags} from "../src/index.js";
+
+const TITLE = "c-forEach.test.ts";
+
+/**
+ * <c:forEach>
+ */
+describe(TITLE, () => {
+    const nsp = createNSP({indent: 4});
+
+    nsp.addTagLib({ns: "c", tag: cTags});
+
+    interface Item {
+        name: string;
+    }
+
+    interface Context {
+        list: Item[];
+        item?: Item;
+    }
+
+    const ctx: Context = {
+        list: [
+            {name: "Foo"},
+            {name: "Bar"},
+            {name: "Buz"},
+            {name: "Qux"},
+            {name: "Quux"},
+        ],
+    };
+
+    it('<c:forEach>', async () => {
+        const src: string = '<c:forEach items="${list}" var="item">[${ item.name }]</c:forEach>';
+        // console.warn(nsp.parse(src).toJS());
+        assert.equal(nsp.parse(src).toFn()(ctx), "[Foo][Bar][Buz][Qux][Quux]");
+    });
+
+    it('<c:forEach begin="1">', async () => {
+        const src: string = '<c:forEach items="${list}" var="item" begin="1" end="3" step="2">[${ item.name }]</c:forEach>';
+        // console.warn(nsp.parse(src).toJS());
+        assert.equal(nsp.parse(src).toFn()(ctx), "[Bar][Qux]");
+    });
+});
